08b722140d9943c103cc080c1d1729031e190133
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-31 01:54:33 -0300

    add new content to home landing

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,32 +1,32 @@
-import { Link, Outlet } from '@solidjs/router'
-
+import { Link } from '@solidjs/router'
+import HeroLandingImage from '../assets/hero-landing.png'
 const HomeLanding = () => {
 	return (
-		<>
-			<header>
-				<nav>
-					<ul>
-						<li></li>
-						<li></li>
-					</ul>
-				</nav>
-			</header>
-			<main class="h-screen">
-				<section class="h-full hero ">
-					<article class="flex-col hero-content lg:flex-row">
-						<aside class="text-center">
-							<h1 class="text-5xl font-bold">
-								Busca tu personaje favorito de Marvel!
-							</h1>
-							<p class="py-6">Clox es una página web</p>
-							<Link href="/characters/home" class="btn btn-secondary">
-								Comenzar ya
-							</Link>
-						</aside>
-					</article>
-				</section>
-			</main>
-		</>
+		<main class="h-screen bg-base-100 font-lato">
+			<section class="h-full hero ">
+				<article class="flex-col hero-content lg:flex-row">
+					<img
+						src={HeroLandingImage}
+						class="rounded-lg border-[1px] border-secondary"
+						height={500}
+						width={500}
+						alt="Imagen principal de la Landing Clox"
+					/>
+					<aside class="text-center">
+						<h1 class="text-4xl font-bold ">
+							Busca tu personaje favorito del universo de comics!
+						</h1>
+						<p class="py-6 text-gray-500">
+							En Clox, puedes elegir y añadir distíntos personajes a tu equipo,
+							ya sea del mundo Marvel, DC y más!
+						</p>
+						<Link href="/characters/home" class="btn btn-secondary">
+							Empieza ya
+						</Link>
+					</aside>
+				</article>
+			</section>
+		</main>
 	)
 }
 
